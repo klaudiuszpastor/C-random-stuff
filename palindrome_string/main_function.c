52a3e02eef1f1907fe86953e3f4982cf6c0130c1
Klaudiusz Pastor <pastor.klo12@gmail.com>
2023-09-06 00:06:44 +0200

    update

--- palindrome_string/main_function.c
+++ palindrome_string/main_function.c
@@ -8,10 +8,15 @@ bool palindrome_string(char string[])
 {
     int middle = strlen(string) / 2;
     int lenght = strlen(string);
+    printf("lenght of string = %d\n",lenght);
 
     for (int i = 0; i <middle; i++)
-    {
+    {   
+        for(int j=0;j<lenght;j++){
+        printf("string[%d = %c] == %c \n",j,string[j],string[lenght - j - 1]);
+        }
         if (string[i] == string[lenght - i - 1])
+        
         {
             return true;
         }
@@ -27,14 +32,14 @@ int main(void)
     char stringi[] = "madam";
     char stringii[] = "racer";
     char stringiii[] = "abc"; 
-
-    if (palindrome_string(stringiii) == true)
+    
+    if (palindrome_string(stringi) == true)
     {
-        printf("%s - is palindrome",stringiii);
+        printf("%s - is palindrome",stringi);
     }
     else 
     {
-        printf("%s - is not palindrome",stringiii);
+        printf("%s - is not palindrome",stringi);
     }
     return 0;
 }
